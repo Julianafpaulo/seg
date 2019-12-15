9825e967384d630880f69c997012d54c4d071174
Rafael <rafams147@gmail.com>
2019-12-15 17:48:56 -0300

    added endpoint and db operations

--- app.js
+++ app.js
@@ -1,14 +1,74 @@
-const http = require('http');
+const express = require('express');
+const mysql = require('mysql');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hello World\n');
+const app = express();
+app.use(express.json());
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'application',
+	password: 'application',
+	database: 'seginfo'
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+
+const student = require('./service/student');
+
+app.get('/', function(req, res) {
+	res.send("Hello World!");
+});
+
+app.get('/students', function(req, res) {
+	student.listStudents(connection)
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => {
+			console.error(err);
+			return res.status(500).send();
+		});
+});
+
+app.get('/student/:id', function(req, res) {
+	student.getStudent(connection, req.params.id)
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => {
+			console.error(err);
+			return res.status(500).send();
+		});
+});
+
+app.post('/student', function(req, res) {
+	student.createStudent(connection, req.body)
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => {
+			console.error(err);
+			return res.status(500).send();
+		});
+});
+
+app.put('/student', function(req, res) {
+	student.editStudent(connection, req.body)
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => {
+			console.error(err);
+			return res.status(500).send();
+		});
+})
+
+app.listen(8080, function() {
+	connection.connect((err) => {
+		if(err){
+			console.error('error connecting: ' + err.stack);
+			return;
+		}
+		
+		console.log('connected as id ' + connection.threadId);
+	});
+	console.log("Example app listening");
 });
---
+++ service/student.js
@@ -0,0 +1,61 @@
+"use strict";
+
+module.exports.listStudents = (connection) => {
+	return new Promise((resolve, reject) => {
+		connection.query('SELECT * FROM students', (error, result, fields) => {
+			if(error){
+				reject(error);
+				return;
+			}
+			resolve(result);
+		});
+	});
+};
+
+module.exports.getStudent = (connection, id) => {
+	return new Promise((resolve, reject) => {
+		connection.query('SELECT * FROM students WHERE id = ? LIMIT 1', [id], (error, result, fields) => {
+			if(error){
+				reject(error);
+				return;
+			}
+			if(result.length > 0)
+				resolve(result[0]);
+			else 
+				resolve({});
+		});
+	});
+};
+
+module.exports.createStudent = (connection, student) => {
+	return new Promise((resolve, reject) => {
+		connection.query(
+			'INSERT INTO students(name, student_registration, email, phone) VALUES (?, ?, ?, ?)', 
+			[student.name, student.student_registration, student.email, student.phone], 
+			(error, result, fields) => {
+				if(error){
+					reject(error);
+					return;
+				}
+				resolve(result);
+			}
+		)
+	});
+};
+
+module.exports.editStudent = (connection, student) => {
+	return new Promise((resolve, reject) => {
+		connection.query(
+			'UPDATE students SET name = ?, student_registration = ?, email = ?, phone = ? WHERE id = ?', 
+			[student.name, student.student_registration, student.email, student.phone, student.id], 
+			(error, result, fields) => {
+				if(error){
+					reject(error);
+					return;
+				}
+				resolve(result);
+			}
+		)
+	});
+};
+
